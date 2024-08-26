f6fc3a4fb0a7da8f540387a54fe3b57116685b90
ANDRE  MARCHIORI <0220482312018@fatectq.local>
2024-08-26 13:53:48 -0300

    Add project files

---
+++ src/main/java/com/github/andremarchiori/CSMRPG.java
@@ -0,0 +1,267 @@
+package com.github.andremarchiori;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
+public class CSMRPG {
+
+	private static Map<Integer, Personagem> personagens;
+	public static void main(String[] args) {
+		personagens = new HashMap<>();
+
+		Boolean loop = true;
+		Scanner scanner = new Scanner(System.in);
+		do {
+
+			chamarMenuPrincipal();
+
+			switch (scanner.nextInt()) {
+			case 1:
+				espacamentoPadrao();
+				File file = new File("characters.txt");
+				System.out.println(" Cadastro de Personagem");
+				System.out.println(";----------------------------------------------------------------;");
+				if (file.exists()) {
+					try {
+						cadastrarPersonagem();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				} else {
+					try {
+						file.createNewFile();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+				break;
+			case 2:
+				espacamentoPadrao();
+				System.out.println(" Listagem de Personagens");
+				System.out.println(";----------------------------------------------------------------;");
+				listarPersonagens();
+				System.out.println(";----------------------------------------------------------------;");
+				break;
+			case 3:
+				espacamentoPadrao();
+				carregarPersonagens();
+				System.out.println(" Adição de XP");
+				System.out.println(";----------------------------------------------------------------;");
+				callAdd(scanner);
+				System.out.println(";----------------------------------------------------------------;");
+				break;
+			case 4:
+				espacamentoPadrao();
+				System.out.println("Finalizado");
+				loop = false;
+				break;
+			default:
+				throw new IllegalArgumentException("A opcao inserida não existe");
+			}
+			espacamentoPadrao();
+		} while (loop == true);
+	}
+
+	public static void callAdd(Scanner scanner) {
+		int idPersonagem;
+		ArrayList<Personagem> listaDePersonagens = new ArrayList<>();
+
+		while (true) {
+			System.out.printf("%nInsira o ID do personagem: ");
+			idPersonagem = scanner.nextInt();
+			if (idPersonagem == -1) {
+				return;
+			} else if (!personagens.containsKey(idPersonagem)) {
+				System.err.println("Id não encontrado. Insira -1 para encerrar ou tente novamente.");
+			} else {
+				break;
+			}
+		}
+
+		carregarPersonagens();
+		System.out.printf("%nInsira o valor de experiência a ser adicionado: ");
+		
+		personagens.get(idPersonagem).addExperience(scanner.nextLong());
+		if (listaDePersonagens.isEmpty()) {
+			
+			try {
+				for(int i = 0; i < proximoId()-1; i++) {
+					listaDePersonagens.add(personagens.get(i+1));
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		clearTheFile();
+		salvarPersonagem(listaDePersonagens);// erro
+		System.out.println("Adição Completa");
+
+	}
+
+	public static void chamarMenuPrincipal() {
+		List<String> menu = new ArrayList<>();
+		menu.add("Sistema de Armazenamento de Personas");
+		menu.add(";----------------------------------------------------------------;");
+		menu.add("Insira uma opcao:");
+		menu.add("1. Cadastro de Personagem");
+		menu.add("2. Listagem de Personagens");
+		menu.add("3. Adicionar XP");
+		menu.add("4. Finalizar");
+		menu.add(";----------------------------------------------------------------;");
+
+		for (String cadaLinha : menu) {
+			System.out.println(cadaLinha);
+		}
+		menu.clear();
+
+		System.out.print("> ");
+	}
+
+	public static void espacamentoPadrao() {
+		System.out.printf("%n%n%n%n");
+	}
+
+	public static void cadastrarPersonagem() throws IOException {
+		Personagem personagem = new Personagem();
+		personagem.setId(proximoId());
+		System.out.println("id = " + personagem.getId());
+		@SuppressWarnings("resource")
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Insira o nome do personagem");
+		personagem.setName(scanner.nextLine());
+
+		System.out.println("Insira o xp do personagem");
+		personagem.setExperience(scanner.nextLong());
+
+		salvarPersonagem(personagem);
+		System.out.println("Cadastro Concluido");
+	}
+
+	private static void carregarPersonagens() {
+		try {
+			BufferedReader bufferedReader = openReaderPersonagens();
+			String linha = bufferedReader.readLine();
+			while (linha != null) {
+				Personagem personagem = parsePersonagem(linha);
+				personagens.put(personagem.getId(), personagem);
+				linha = bufferedReader.readLine();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static Personagem parsePersonagem(String linha) {
+		String[] colunas = linha.split(";");
+		Personagem personagem = new Personagem();
+		personagem.setId(Integer.parseInt(colunas[0]));
+		personagem.setName(colunas[1]);
+		personagem.setLevel(Integer.parseInt(colunas[2]));
+		personagem.setExperience(Long.parseLong(colunas[3]));
+		return personagem;
+	}
+
+	public static int proximoId() throws IOException {
+		int maiorId = 0;
+
+		BufferedReader bufferedReader = openReaderPersonagens();
+		String linha = bufferedReader.readLine();
+		while (linha != null) {
+			String[] colunas = linha.split(";");
+			maiorId = obtencaoDeMaiorID(maiorId, linha, colunas[0]);
+			linha = bufferedReader.readLine();
+		}
+		bufferedReader.close();
+
+		return ++maiorId;
+	}
+
+	private static int obtencaoDeMaiorID(int maiorId, String linha, String coluna) {
+		try {
+			var id = Integer.parseInt(coluna);
+			if (id > maiorId) {
+				maiorId = id;
+			}
+		} catch (NumberFormatException e) {
+			System.err.println("falha ao fazer parse da linha: [" + linha + "], falha: [" + e.getMessage() + "]");
+			e.printStackTrace();
+		}
+		return maiorId;
+	}
+
+	private static void salvarPersonagem(Personagem personagem) {
+		try {
+			BufferedWriter bufferedWriter = openWriterPersonagens();
+			bufferedWriter.write(personagem.toString());
+			bufferedWriter.newLine();
+			bufferedWriter.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static void salvarPersonagem(ArrayList<Personagem> listaDePersonagens) {
+		try {
+			BufferedWriter bufferedWriter = openWriterPersonagens();
+			for (int j = 0; j < listaDePersonagens.size(); j++) {
+				bufferedWriter.write(listaDePersonagens.get(j).toString());
+				bufferedWriter.newLine();
+				bufferedWriter.close();
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static void listarPersonagens() {
+		try {
+			BufferedReader bufferedReader = openReaderPersonagens();
+			String linhas = bufferedReader.readLine();
+			System.out.printf("%n  %-5s%-35s%-10s%-12s%n", "ID", "NOME", "LEVEL", "EXPERIENCIA");
+			while (linhas != null) {
+				String[] colunas = linhas.split(";");
+				System.out.println(
+						String.format("  %-5s%-35s%-10s%-12s", colunas[0], colunas[1], colunas[2], colunas[3]));
+				linhas = bufferedReader.readLine();
+			}
+
+			bufferedReader.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static BufferedWriter openWriterPersonagens() throws IOException {
+		FileWriter escreveDados = new FileWriter("characters.txt", true);
+		return new BufferedWriter(escreveDados);
+	}
+
+	private static BufferedReader openReaderPersonagens() throws FileNotFoundException {
+		FileReader leitor = new FileReader("characters.txt");
+		return new BufferedReader(leitor);
+	}
+
+	private static void clearTheFile() {
+		try {
+			FileWriter fwOb = new FileWriter("characters.txt", false);
+			PrintWriter pwOb = new PrintWriter(fwOb, false);
+			pwOb.flush();
+			pwOb.close();
+			fwOb.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ src/main/java/com/github/andremarchiori/Personagem.java
@@ -0,0 +1,183 @@
+package com.github.andremarchiori;
+
+public class Personagem {
+	private int id;
+
+	private int level = 1;
+	private long experience = 0;
+	private String name;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Personagem() {
+	}
+
+	public int getLevel() {
+		return level;
+	}
+
+	public void setLevel(int level) {
+		this.level = level;
+	}
+
+	public long getExperience() {
+		return experience;
+	}
+
+	public void setExperience(long experience) {
+		this.experience = experience;
+		refreshLevel(this.level, this.experience);
+	}
+
+	public void addExperience(long experience) {
+		this.experience += experience;
+		refreshLevel(this.level, this.experience);
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	private void refreshLevel(int level, long experience) {
+		switch (level) {
+		case 1:
+			if (experience >= 300) {
+				levelUp();
+				refreshLevel(this.level, this.experience);
+			}
+			break;
+		case 2:
+			if (experience >= 900) {
+				levelUp();
+				refreshLevel(this.level, this.experience);
+			}
+			break;
+		case 3:
+			if (experience >= 2700) {
+				levelUp();
+				refreshLevel(this.level, this.experience);
+			}
+			break;
+		case 4:
+			if (experience >= 6500) {
+				levelUp();
+				;
+				refreshLevel(this.level, this.experience);
+			}
+			break;
+		case 5:
+			if (experience >= 14000) {
+				levelUp();
+				refreshLevel(this.level, this.experience);
+			}
+			break;
+		case 6:
+			if (experience >= 23000) {
+				levelUp();
+				refreshLevel(this.level, this.experience);
+			}
+			break;
+		case 7:
+			if (experience >= 34000) {
+				levelUp();
+				refreshLevel(this.level, this.experience);
+			}
+			break;
+		case 8:
+			if (experience >= 48000) {
+				levelUp();
+				refreshLevel(this.level, this.experience);
+			}
+			break;
+		case 9:
+			if (experience >= 64000) {
+				levelUp();
+				refreshLevel(this.level, this.experience);
+			}
+			break;
+		case 10:
+			if (experience >= 85000) {
+				levelUp();
+				refreshLevel(this.level, this.experience);
+			}
+			break;
+		case 11:
+			if (experience >= 100000) {
+				levelUp();
+				refreshLevel(this.level, this.experience);
+			}
+			break;
+		case 12:
+			if (experience >= 120000) {
+				levelUp();
+				refreshLevel(this.level, this.experience);
+			}
+			break;
+		case 13:
+			if (experience >= 140000) {
+				levelUp();
+				refreshLevel(this.level, this.experience);
+			}
+			break;
+		case 14:
+			if (experience >= 165000) {
+				levelUp();
+				refreshLevel(this.level, this.experience);
+			}
+			break;
+		case 15:
+			if (experience >= 195000) {
+				levelUp();
+				refreshLevel(this.level, this.experience);
+			}
+			break;
+		case 16:
+			if (experience >= 225000) {
+				levelUp();
+				refreshLevel(this.level, this.experience);
+			}
+			break;
+		case 17:
+			if (experience >= 265000) {
+				levelUp();
+				refreshLevel(this.level, this.experience);
+			}
+			break;
+		case 18:
+			if (experience >= 305000) {
+				levelUp();
+				refreshLevel(this.level, this.experience);
+			}
+			break;
+		case 19:
+			if (experience >= 355000) {
+				levelUp();
+				refreshLevel(this.level, this.experience);
+			}
+			break;
+		case 20:
+			break;
+		default:
+			System.err.println("Not valid level value");
+		}
+	}
+
+	private void levelUp() {
+		this.level += 1;
+	}
+	
+	@Override
+	public String toString() {
+		return String.format("%d;%s;%d;%d", this.id, this.name, this.level, this.experience);
+	}
+}
